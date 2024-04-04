79f21270213a884b136be9d9ad1b0befe8992571
luiz-ockner <140465430+luiz-ockner@users.noreply.github.com>
2024-04-04 01:26:36 +0000

    finish `/api/v1/status` endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -2,9 +2,26 @@ import database from "infra/database.js";
 
 async function status(request,response)
 {
-   const result = await database.query('Select 1 + 1 as SUM;');
-   console.log(result.rows);
-  response.status(200).json({chave:"São acima da média."});
+  const database_values = await database.query('SHOW server_version;');  
+  const dbVersion = database_values.rows[0].server_version;
+  const dbMaxConn = await database.query('SHOW max_connections;'); 
+  const dbname = process.env.POSTGRES_DB;
+  const dbActivity = await database.query({
+    text:"SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",
+    values:[dbname],}
+    );
+  const updateAt = new Date().toISOString();
+    
+  response.status(200).json({
+    dependencies:{
+      database:{
+        version : dbVersion
+        ,max_conn : parseInt(dbMaxConn.rows[0].max_connections)
+      }
+    },
+    updated_at: updateAt,    
+    activity : dbActivity.rows[0].count
+  });
 }
 
 export default status;
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,4 +1,12 @@
 test("GET to /api/v1/status should return 200",async() => {
   const response = await fetch("http://localhost:3000/api/v1/status");
   expect(response.status).toBe(200);
+
+  const responseBody = await response.json();
+  expect(responseBody.updated_at).toBeDefined();
+  expect(responseBody.dependencies.database.max_conn).toEqual(100);
+  expect(responseBody.activity).toEqual(1);
+    
+  new Date(responseBody.updated_at).toISOString();
+
 });
