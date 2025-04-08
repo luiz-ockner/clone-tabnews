0692cac220d92913852021e7138733cd68775f14
luiz-ockner <140465430+luiz-ockner@users.noreply.github.com>
2025-04-08 09:20:51 +0000

    style: change style `/status` to format prettier

--- pages/status/index.js
+++ pages/status/index.js
@@ -1,13 +1,13 @@
 import useSWR from "swr";
 
-async function fetchAPI(key){
+async function fetchAPI(key) {
   const response = await fetch(key);
   const responseBody = await response.json();
   return responseBody;
 }
 
-export default function StatusPage(){
-   return (
+export default function StatusPage() {
+  return (
     <>
       <h1>Status</h1>
       <UpdatedAt />
@@ -16,41 +16,48 @@ export default function StatusPage(){
   );
 }
 
-function UpdatedAt(){
-  const { isLoading,data} = useSWR("/api/v1/status",fetchAPI, {
+function UpdatedAt() {
+  const { isLoading, data } = useSWR("/api/v1/status", fetchAPI, {
     refreshInterval: 2000,
     dedupingInterval: 2000,
   });
 
   let updatedAtText = "Carregando...";
 
-  if(!isLoading && data){
+  if (!isLoading && data) {
     updatedAtText = new Date(data.updated_at).toLocaleString("pt-BR", {
       timeZone: "America/Sao_Paulo",
       hour12: false,
     });
   }
-  
-  
-  return <div>Última atualização: {updatedAtText}</div>
-};
 
-function DatabaseStatus(){
-  const { isLoading,data} = useSWR("/api/v1/status",fetchAPI, {
+  return <div>Última atualização: {updatedAtText}</div>;
+}
+
+function DatabaseStatus() {
+  const { isLoading, data } = useSWR("/api/v1/status", fetchAPI, {
     refreshInterval: 2000,
     dedupingInterval: 2000,
   });
 
   let databaseStatusText = "Carregando...";
 
-  if(!isLoading && data){
-    databaseStatusText = 
+  if (!isLoading && data) {
+    databaseStatusText = (
       <>
         <div> Versão: {data.dependencies.database.version}</div>
         <div> Máximo de conexões: {data.dependencies.database.max_conn}</div>
-        <div> Conexões Abertas: {data.dependencies.database.opened_connections}</div>
-     </>
+        <div>
+          {" "}
+          Conexões Abertas: {data.dependencies.database.opened_connections}
+        </div>
+      </>
+    );
   }
-  
-  return <div><h1>Banco de dados</h1> {databaseStatusText}</div>
+
+  return (
+    <div>
+      <h1>Banco de dados</h1> {databaseStatusText}
+    </div>
+  );
 }
